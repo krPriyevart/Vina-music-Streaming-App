8dcc0b0d4b1d7e2758a0aa84e0a8d81fba59a015
krpriyevart <kumarpriyevart.com>
2024-11-25 23:21:36 +0530

    Sign Up and Sign In UI

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,5 +1,6 @@
 import {BrowserRouter, Routes, Route} from "react-router-dom";
 import Login from "./routes/login.jsx";
+import Register from "./routes/register.jsx";
 import Home from "./routes/home.jsx";
 function App() {
 
@@ -9,7 +10,7 @@ function App() {
       <BrowserRouter>
       <Routes>
         <Route path="/" element={<Home />}></Route>
-        <Route path="/register" element={<div>register</div>}></Route>
+        <Route path="/register" element={<Register />}></Route>
         <Route path="/login" element={<Login />}></Route>
         <Route path="/profile" element={<div>profile</div>}></Route>
       </Routes>
--- client/src/routes/register.jsx
+++ client/src/routes/register.jsx
@@ -1,13 +1,14 @@
-import React from "react";
+import React from 'react'
 import Vinna from "../assets/Vinna.png";
 import GoogleLogo from "../assets/Google__logo.webp";
-function Login() {
+
+function register() {
   return (
     <>
       <div className="w-full h-screen bg-slate-400 flex justify-center items-center">
         <div className="w-1/2 h-3/4 bg-slate-500 flex rounded">
-          <div className="w-1/2 h-full bg-[#F7F7F5] rounded">
-            <div className="w-full h-auto mt-7 border-0 border-sky-500 ">
+          <div className="w-1/2 h-full bg-[#F7F7F5]">
+            <div className="w-full h-auto mt-5 border-0 border-sky-500 ">
               <div className="w-full h-auto border-0 border-sky-500 flex justify-center">
                 <img src={Vinna} className="w-1/3" />
               </div>
@@ -16,10 +17,16 @@ function Login() {
               </div>
             </div>
             <div className="w-full h-4/5 border-0 border-red-500 ">
-              <div className="w-full h-auto border-0 px-5 mt-5 border-sky-500 flex justify-start text-gray-700">
-                <h1 className="text-5xl"> Sign In </h1>
+              <div className="w-full h-auto border-0 px-5 mt-2 border-sky-500 flex justify-start text-gray-700">
+                <h1 className="text-5xl"> Sign Up</h1>
               </div>
-              <div className="w-full h-auto border-0 px-5 mt-5 border-sky-500 flex justify-start text-gray-700">
+              <div className="w-full h-auto border-0 px-5 mt-4 border-sky-500 flex justify-start text-gray-700">
+                <p>Name</p>
+              </div>
+              <div className="w-full h-auto border-0 px-5 border-sky-500 flex justify-start text-gray-700">
+                <input type="text" className="w-4/5 p-2 shadow-md outline-0 font-mono text-lg rounded-sm" />
+              </div>
+              <div className="w-full h-auto border-0 px-5 mt-4 border-sky-500 flex justify-start text-gray-700">
                 <p>Email</p>
               </div>
               <div className="w-full h-auto border-0 px-5 border-sky-500 flex justify-start text-gray-700">
@@ -31,21 +38,18 @@ function Login() {
               <div className="w-full h-auto border-0 px-5 border-sky-500 flex justify-start text-gray-700">
                 <input type="password" className="w-4/5 p-2 shadow-md outline-0 font-mono text-lg rounded-sm"/>
               </div>
-              <div className="w-full h-auto border-0 px-20 border-sky-500 flex justify-end text-gray-700">
-                <p className="cursor-pointer">Forget password</p>
-              </div>
               <div className="w-full h-auto border-0 px-5 my-5  border-sky-500 flex justify-start ">
                 <button className="w-4/5 p-2 shadow-md outline-0 font-mono text-lg rounded-sm bg-[#ff4646] text-white">Submit</button>
               </div>
               <div className="w-full h-auto border-0 px-5 my-4 border-sky-500 flex justify-center text-gray-700">
-                <p>Or Sign in with</p>
+                <p>Or Sign Up with</p>
               </div>
-              <div className="w-3/4 mx-12 h-auto border-0 px-5 cursor-pointer bg-white py-2 rounded-3xl border-sky-500 flex justify-center text-gray-700">
+              <div className="w-3/4 mx-12 h-auto border-0 px-5 bg-white shadow py-2 rounded-3xl border-sky-500 flex justify-center text-gray-700 cursor-pointer">
                 <img src={GoogleLogo} className="w-5 h-5 " />{" "}
                 <p className="px-5">Continue with Google</p>
               </div>
               <div className="w-full h-auto border-0 px-5 my-4 border-sky-500 flex justify-center text-gray-700">
-                <p>don't have account - <b className="cursor-pointer">Sign Up</b></p>
+                <p>Already have an account - <b className='cursor-pointer'>Sign In</b></p>
               </div>
             </div>
           </div>
@@ -53,7 +57,7 @@ function Login() {
         </div>
       </div>
     </>
-  );
+  )
 }
 
-export default Login;
+export default register
